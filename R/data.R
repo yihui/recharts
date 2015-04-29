6d983f6646c0156e9d8f9d331ca82a4d40129a64
Yihui Xie <xie@yihui.name>
2015-04-29 17:44:09 -0500

    consider the case of x = factor and y = NULL for bar plots: we plot the frequencies of x

--- R/data.R
+++ R/data.R
@@ -11,21 +11,42 @@ data_scatter = function(x, y, series = NULL, type = 'scatter') {
   obj
 }
 
-data_bar = function(x, y, series = NULL) {
-  ### if no series passed... go with only one series.
-  if (is.null(series)) {
-    warning("No series specified for bar plot.")
-    return(list(list(name = '', type = 'bar', data = y)))
+data_bar = function(x, y, series = NULL, type = 'bar') {
+
+  # plot the frequencies of x when y is not provided
+  if (is.null(y)) {
+
+    if (is.null(series)) {
+      y = table(x)
+      return(list(list(type = type, data = unname(c(y)))))
+    }
+
+    y = table(x, series)
+    nms = colnames(y)
+    obj = list()
+    for (i in seq_len(ncol(y))) {
+      obj[[i]] = list(name = nms[i], type = type, data = unname(y[, i]))
+    }
+    return(obj)
+
   }
-  #otherwise, go with series.
-  xy = y ### why no names?
-  xy = split(as.data.frame(xy), series)
-  nms = names(xy)
+
+  # when y is provided, use y as the height of bars
+  if (is.null(series)) {
+    return(list(list(type = type, data = y)))
+  }
+
+  xy = tapply(y, list(x, series), function(z) {
+    if (length(z) == 1) return(z)
+    stop('y must only have one value corresponding to each combination of x and series')
+  })
+  nms = colnames(xy)
   obj = list()
-  for (i in seq_along(xy)) {
-    obj[[i]] = list(name = nms[i], type = 'bar', data = unname(unlist(xy[[i]])))
+  for (i in seq_len(ncol(xy))) {
+    obj[[i]] = list(name = nms[i], type = type, data = unname(xy[, i]))
   }
   obj
+
 }
 
 data_line = function(x, y, series = NULL) {

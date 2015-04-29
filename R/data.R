3453063213b34d27675dde43bee391dbb1e06030
Yihui Xie <xie@yihui.name>
2015-04-29 17:42:42 -0500

    do not hard-code the type `scatter`, so we can reuse this function in data_line() later

--- R/data.R
+++ R/data.R
@@ -1,12 +1,12 @@
 # split the data matrix for a scatterplot by series
-data_scatter = function(x, y, series = NULL) {
+data_scatter = function(x, y, series = NULL, type = 'scatter') {
   xy = unname(cbind(x, y))
-  if (is.null(series)) return(list(list(type = 'scatter', data = xy)))
+  if (is.null(series)) return(list(list(type = type, data = xy)))
   xy = split(as.data.frame(xy), series)
   nms = names(xy)
   obj = list()
   for (i in seq_along(xy)) {
-    obj[[i]] = list(name = nms[i], type = 'scatter', data = unname(as.matrix(xy[[i]])))
+    obj[[i]] = list(name = nms[i], type = type, data = unname(as.matrix(xy[[i]])))
   }
   obj
 }
